eccbde665ef7e50ed242ec1c6e5254d0072cfecd
Rick Anderson <rianders@docs.rutgers.edu>
2025-01-08 14:06:51 -0500

    more vite clarfications

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import StickyOrganizer from './components/StickyOrganizer'
+
+function App() {
+  return (
+    <div>
+      <StickyOrganizer />
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-// src/index.js
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import StickyOrganizer from './StickyOrganizer';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<StickyOrganizer />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
